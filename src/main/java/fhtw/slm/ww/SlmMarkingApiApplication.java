5c90dab9c9bb1a8614943d87797d6355174721af
yukarisusaki <yukarisoprano717@gmail.com>
2022-12-21 18:05:00 +0100

    Initial Commit

---
+++ src/main/java/fhtw/slm/ww/SlmMarkingApiApplication.java
@@ -0,0 +1,13 @@
+package fhtw.slm.ww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmMarkingApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SlmMarkingApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/fhtw/slm/ww/SlmMarkingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.slm.ww;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmMarkingApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
